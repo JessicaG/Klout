ae7f2de6e0933cdfe48ee8db781845232c385135
Jessica Goulding <jgoulding2@gmail.com>
2012-02-08 13:02:31 -0700

    kloutscore.rb with sinatra wrap

--- kloutscore.rb
+++ kloutscore.rb
@@ -4,6 +4,7 @@ get '/' do
   'Hello world!'
 end
 
+get '/' do
 klout git:(master) k = Klout::API.new('wp4e3hxhbkktkyd45k59b9nk')
 k = Klout::API.new('wp4e3hxhbkktkyd45k59b9nk',{:format => 'xml',:secure=>true})
 
@@ -15,3 +16,4 @@ k.topics('dhh')
 
 k.influencer_of('rails')
 k.influenced_by('dhh, rails')
+end
