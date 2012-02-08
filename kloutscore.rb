9861779d48a2db34e00e6e4d898ac30aefe7082b
Jessica Goulding <jgoulding2@gmail.com>
2012-02-08 12:48:48 -0700

    edited kloutscore.rb with API

--- kloutscore.rb
+++ kloutscore.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+klout git:(master) k = Klout::API.new('wp4e3hxhbkktkyd45k59b9nk')
+k = Klout::API.new('wp4e3hxhbkktkyd45k59b9nk',{:format => 'xml',:secure=>true})
+
+k.klout('dhh')
+k.klout('dhh, rails')
+
+k.show('dhh')
+k.topics('dhh')
+
+k.influencer_of('rails')
+k.influenced_by('dhh, rails')
