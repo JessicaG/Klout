ed9d4369b4211a433de1a7b0359c79ed64eb8727
Jessica Goulding <jgoulding2@gmail.com>
2012-02-09 18:08:43 -0700

    updated files

--- kloutscore.rb
+++ kloutscore.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'klout'
 require 'json'
+
 k = Klout::API.new('wp4e3hxhbkktkyd45k59b9nk')
 
 get '/' do
@@ -8,8 +9,23 @@ get '/' do
 end
 
 post '/' do
-  content_type :json
-  twitter_handle = params[:twitter_handle]
-  k.klout(twitter_handle).to_json 
+  
+  
+  #k.show(twitter_handle).to_json 
+  #k.topics(twitter_handle).to_json 
 
+  #k.influencer_of(twitter_handle).to_json 
+  #k.influenced_by(twitter_handle).to_json 
+  
+  #if params twitter_handle is empty, execute file upload
+   
+  unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
+    twitter_handle = params[:twitter_handle] 
+     @score = JSON.parse(k.klout(twitter_handle).to_json)
+     return erb :score, {:locals => @score}
+  end
+  contents = tmpfile.read
+  #ruby for inserting commas between names, send to klout API then to output. 
+  
+ 
 end 
